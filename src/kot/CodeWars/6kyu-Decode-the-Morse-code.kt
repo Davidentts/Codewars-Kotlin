7da6c33c95c5cbfea7426ae69117b189e2e5b6c9
Davidentt <tsgoev.david.s@gmail.com>
2023-10-28 12:50:12 +0300

    Decode the Morse code

---
+++ src/kot/CodeWars/6kyu-Decode-the-Morse-code.kt
@@ -0,0 +1,18 @@
+package kot.CodeWars
+
+fun decodeMorse(code: String): String {
+    val listStr = code
+        .trim(' ')
+        .trimEnd(' ')
+        .replace("   ", "  ")
+        .split(" ")
+    var result = ""
+    for(str in listStr){
+        if(str.isEmpty()){
+            result = "$result "
+            continue
+        }
+        result = "$result${MorseCode[str] ?: ""}"
+    }
+    return result
+}
